289b8ac64b78e8d495968fa218fa073b2b8bfd4d
ArtomStatsenko <namebyuser@gmail.com>
2022-07-11 18:08:47 +0300

    feat: Add patrol command logic

--- Assets/Scripts/Core/Units/UnitCommandExecutors/PatrolCommandExecutor.cs
+++ Assets/Scripts/Core/Units/UnitCommandExecutors/PatrolCommandExecutor.cs
@@ -1,12 +1,45 @@
+using System.Threading;
 using System.Threading.Tasks;
 using Abstractions.Commands.CommandInterfaces;
 using UnityEngine;
+using UnityEngine.AI;
+using Utils;
 
 namespace Core.Units.UnitCommandExecutors
 {
     public class PatrolCommandExecutor : CommandExecutorBase<IPatrolCommand>
     {
-        public override async Task ExecuteSpecificCommand(IPatrolCommand command) =>
-            Debug.Log($"Patrolling from ({command.From}) to ({command.To})");
+        [SerializeField] private UnitMovementStop _stop;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private StopCommandExecutor _stopCommandExecutor;
+
+        public override async Task ExecuteSpecificCommand(IPatrolCommand command)
+        {
+            var point1 = command.From;
+            var point2 = command.To;
+
+            while (true)
+            {
+                GetComponent<NavMeshAgent>().destination = point2;
+                _animator.SetTrigger(Animator.StringToHash(AnimationTypes.Walk));
+                _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+
+                try
+                {
+                    await _stop.WithCancellation(_stopCommandExecutor.CancellationTokenSource.Token);
+                }
+                catch
+                {
+                    GetComponent<NavMeshAgent>().isStopped = true;
+                    GetComponent<NavMeshAgent>().ResetPath();
+                    break;
+                }
+
+                (point1, point2) = (point2, point1);
+            }
+
+            _stopCommandExecutor.CancellationTokenSource = null;
+            _animator.SetTrigger(Animator.StringToHash(AnimationTypes.Idle));
+        }
     }
 }
